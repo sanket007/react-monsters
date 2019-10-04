09e9dfbc4908abd4bf26746b3ae488f2ff34dc6b
sanket_ebiz <sanket.lakhani@esparkbizmail.com>
2019-10-04 11:53:03 +0530

    Updates

---
+++ precache-manifest.9b94a228c07afcf22e6bef51666e7d40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a14a9831c0a5dc2be8a9e071944d309",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "2ea24959171c9a427e4c",
+    "url": "/react-monsters/static/css/main.83b90da7.chunk.css"
+  },
+  {
+    "revision": "076a7f1ef25fedad09fd",
+    "url": "/react-monsters/static/js/2.fcf601c9.chunk.js"
+  },
+  {
+    "revision": "2ea24959171c9a427e4c",
+    "url": "/react-monsters/static/js/main.6fcc9edf.chunk.js"
+  },
+  {
+    "revision": "c8a21d4ffd1ea64381e9",
+    "url": "/react-monsters/static/js/runtime-main.3b082982.js"
+  }
+]);
